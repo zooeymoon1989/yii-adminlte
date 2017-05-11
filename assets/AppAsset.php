e93681806af2367eddd1f7f104fa98e096c0f764
LiWenQiang <wenqiang.li@net263.com>
2017-05-11 17:28:28 +0800

    before delete src folder Signed-off-by: LiWenQiang <wenqiang.li@net263.com>

---
+++ assets/AppAsset.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@webroot/src';
+    public $css = [
+        'css/site.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        'dist/css/AdminLTE.min.css',
+        'dist/css/skins/_all-skins.min.css'
+    ];
+    public $js = [
+        'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js',
+        'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
+        'bootstrap/js/bootstrap.min.js',
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/fastclick/fastclick.js',
+        'dist/js/app.min.js',
+        'dist/js/demo.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
